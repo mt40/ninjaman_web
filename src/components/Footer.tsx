42078285f95ea8609dfdcc9485ee67e8c0aa1318
minhthai <minhthai40@gmail.com>
2019-07-19 22:42:56 +0700

    Add about us

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,54 @@
 import React from 'react'
 import Container from './Container'
-import { getImage, isMobile } from '../util/Resource'
+import { isMobile } from '../util/Resource'
 import './Footer.css'
 import SimpleButton from './SimpleButton'
+import { T } from '../config/translation/util'
 
 const Footer: React.FC = () => {
+  const [isAboutUsVisible, setIsAboutUsVisible] = React.useState(false)
+
+  const aboutUs = () => {
+    const isActive = isAboutUsVisible ? 'is-active' : ''
+    return (
+      <div className={ `modal ${ isActive }` }>
+        <div className="modal-background" onClick={ toggleAboutUs }/>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">{ T('About Us') }</p>
+            <button className="delete" aria-label="close" onClick={ toggleAboutUs }/>
+          </header>
+          <section className="modal-card-body">
+            <p>
+              {
+                T('Belazy is a marketplace for for home beauty services, based in Ho Chi Minh city. ' +
+                  'We help customers hire trusted professionals for all your service needs ' +
+                  'at your own home.')
+              }
+            </p>
+            <br/>
+            <p>
+              {
+                T('If you need support, please call us at 0909882266 or email us ' +
+                  'at ')
+              }
+              <a href="mailto: contact@belazy.vn">contact@belazy.vn</a>
+            </p>
+          </section>
+        </div>
+      </div>
+    )
+  }
+
+  const toggleAboutUs = () => {
+    setIsAboutUsVisible(!isAboutUsVisible)
+  }
+
   return (
     <footer className="Footer footer">
       <Container>
+        { aboutUs() }
+
         <div className="columns">
           <div className="column is-4">
             <div className="company_info">
@@ -26,8 +67,10 @@ const Footer: React.FC = () => {
               flexDirection: 'column',
               alignItems: isMobile() ? 'flex-start' : 'flex-end',
             } }>
-              <SimpleButton style={ {minWidth: 'auto'} } text="About Us" lightColor={ true }/>
-              <SimpleButton style={ {minWidth: 'auto'} } text="Terms & Conditions" lightColor={ true }/>
+              <SimpleButton style={ {minWidth: 'auto'} } text="About Us" lightColor={ true }
+                            onClick={ toggleAboutUs }/>
+              <SimpleButton style={ {minWidth: 'auto'} } text="Terms & Conditions"
+                            lightColor={ true }/>
             </div>
           </div>
         </div>
--- src/components/SimpleButton.tsx
+++ src/components/SimpleButton.tsx
@@ -1,20 +1,20 @@
-import React, { CSSProperties } from 'react'
+import React, { CSSProperties, DOMAttributes, HTMLAttributes } from 'react'
 import './SimpleButton.css'
 
-interface SimpleButtonProps {
+interface SimpleButtonProps extends HTMLAttributes<HTMLElement> {
   text: string,
   lightColor?: boolean
-  style?: CSSProperties
 }
 
 const SimpleButton: React.FC<SimpleButtonProps> = (props) => {
-  const colorClass = props.lightColor ? 'has-text-white-ter' : ''
+  const {text, lightColor, ...others} = props
+  const colorClass = lightColor ? 'has-text-white-ter' : ''
 
   return (
     // todo: href
-    <a className={ `SimpleButton button no_bg ${ colorClass }` } href="/"
-       style={ props.style }>
-      { props.text }
+    <a className={ `SimpleButton button no_bg ${ colorClass }` }
+       { ...others }>
+      { text }
     </a>
   )
 }
