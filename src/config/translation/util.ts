86818fa8d467132a575dd9ad45b7e4caf8b8c19c
minhthai <minhthai40@gmail.com>
2019-07-20 10:21:38 +0700

    Disable translation

--- src/config/translation/util.ts
+++ src/config/translation/util.ts
@@ -1,11 +1,8 @@
-import vn from '../vn'
-
 // enum Lang {
 //   VN = 0,
 //   EN = 1,
 // }
 
 export const T = (id: string) => {
-  const rs = (vn as Record<string, string>)[id]
-  return rs ? rs : id
+  return id
 }
