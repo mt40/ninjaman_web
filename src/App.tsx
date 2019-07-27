16365382d2e66be9d37312ab0a270b10386074cd
minhthai <minhthai40@gmail.com>
2019-07-27 22:54:31 +0700

    Refactor context

--- src/App.tsx
+++ src/App.tsx
@@ -4,185 +4,19 @@ import HomePage from './components/home/HomePage'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ListingDetailPage from './components/listing_detail/ListingDetailPage'
 import TopBar from './components/TopBar'
-import * as _ from 'lodash'
-import { none, Option, some } from 'ts-option'
-import { ServiceContext } from './context/service'
-import { groupOf, QueryInfo, ServiceInfo } from './config/services'
-import { Cart } from './models/Cart'
-import { MyContext } from './models/MyContext'
-import { QueryAnswer } from './models/QueryAnswer'
-import { UserInfo } from './models/UserInfo'
-import { Lang } from './models/Lang'
+import { contextFor, defaultContext, MyContext } from './models/MyContext'
 import { ReactCookieProps, withCookies } from 'react-cookie'
 
-// export const appContext = React.createContext<AppContext>(defaultContext)
-// const Provider = appContext.Provider
-
-export const appContext = React.createContext<MyContext>({
-  data: {
-    query: {
-      answers: [] as QueryAnswer[],
-    },
-    service: none as Option<ServiceContext>,
-    cart: new Cart(),
-    user: UserInfo.default,
-    lang: Lang.VN,
-  },
-  action: {
-    answer: () => {},
-    popAnswer: () => {},
-    clearAnswers: () => {},
-    getNextQuery: () => none,
-    setService: () => {},
-    setCart: () => {},
-    clearCart: () => {},
-    setUser: () => {},
-    setLang: () => {},
-  },
-})
+export const appContext = React.createContext<MyContext>(defaultContext)
 const Provider = appContext.Provider
 
-interface Props extends ReactCookieProps {}
+export interface AppProps extends ReactCookieProps {}
 
-class App extends React.Component<Props, MyContext> {
-  getLang() {
-    if(this.props.cookies) {
-      const fromCookie = this.props.cookies.get('lang')
-      return fromCookie ? Lang.fromName(fromCookie) : Lang.VN
-    }
-    return Lang.VN
-  }
+class App extends React.Component<AppProps, MyContext> {
+  state: MyContext = defaultContext
 
-  state: MyContext = {
-    data: {
-      query: {
-        answers: [],
-      },
-      service: none,
-      cart: new Cart(),
-      user: UserInfo.default,
-      lang: this.getLang(),
-    },
-    action: {
-      answer: (query: QueryInfo, ...answers: string[]) => {
-        const newState = {
-          ...this.state,
-          data: {
-            ...this.state.data,
-            query: {
-              answers: this.state.data.query.answers.concat(
-                answers.map(a => {
-                  return {query: query, get: a}
-                }),
-              ),
-            },
-          },
-        }
-        if (!_.isEqual(newState.data, this.state.data)) {
-          this.setState(newState)
-        }
-      },
-      popAnswer: () => {
-        const newState = {
-          ...this.state,
-          data: {
-            ...this.state.data,
-            query: {
-              answers: _.dropRight(this.state.data.query.answers),
-            },
-            cart: new Cart(),
-          },
-        }
-        if (!_.isEqual(newState.data, this.state.data)) {
-          this.setState(newState)
-        }
-      },
-      clearAnswers: () => {
-        const newState = {
-          ...this.state,
-          data: {
-            ...this.state.data,
-            query: {
-              answers: [],
-            },
-            cart: new Cart(),
-          },
-        }
-        if (!_.isEqual(newState.data, this.state.data)) {
-          this.setState(newState)
-        }
-      },
-      getNextQuery: (): Option<QueryInfo> => {
-        const next = this.state.data.query.answers.length
-        return this.state.data.service.map((sv: ServiceContext) => {
-          return sv.info.queries[next]
-        })
-      },
-      setService: (sv: ServiceInfo) => {
-        const newState = {
-          ...this.state,
-          data: {
-            ...this.state.data,
-            service: some({
-              group: groupOf(sv),
-              info: sv,
-            }),
-          },
-        }
-        if (!_.isEqual(newState.data, this.state.data)) {
-          this.setState(newState)
-        }
-      },
-      setCart: (lastAns: string, count: number) => {
-        const chain = this.state.data.query.answers.map(a => a.get).concat([lastAns])
-        const newState = {
-          ...this.state,
-          data: {
-            ...this.state.data,
-            cart: this.state.data.cart.set(chain, count),
-          },
-        }
-        if (!_.isEqual(newState.data, this.state.data)) {
-          this.setState(newState)
-        }
-      },
-      clearCart: () => {
-        const newState = {
-          ...this.state,
-          data: {
-            ...this.state.data,
-            cart: new Cart(),
-          },
-        }
-        if (!_.isEqual(newState.data, this.state.data)) {
-          this.setState(newState)
-        }
-      },
-      setUser: (user: UserInfo) => {
-        const newState = {
-          ...this.state,
-          data: {
-            ...this.state.data,
-            user: user,
-          },
-        }
-        if (!_.isEqual(newState.data, this.state.data)) {
-          this.setState(newState)
-        }
-      },
-      setLang: (lang: Lang) => {
-        const newState = {
-          ...this.state,
-          data: {
-            ...this.state.data,
-            lang: lang,
-          },
-        }
-        if (!_.isEqual(newState.data, this.state.data)) {
-          this.setState(newState)
-        }
-      },
-    },
+  componentWillMount(): void {
+    this.setState(contextFor(this))
   }
 
   render() {
--- src/models/MyContext.ts
+++ src/models/MyContext.ts
@@ -1,10 +1,13 @@
-import { Option } from 'ts-option'
+import { none, Option, some } from 'ts-option'
 import { ServiceContext } from '../context/service'
 import { Cart } from './Cart'
-import { QueryInfo, ServiceInfo } from '../config/services'
+import { groupOf, QueryInfo, ServiceInfo } from '../config/services'
 import { QueryAnswer } from './QueryAnswer'
 import { UserInfo } from './UserInfo'
 import { Lang } from './Lang'
+import * as _ from 'lodash'
+import React from 'react'
+import { AppProps } from '../App'
 
 export interface MyContext {
   data: {
@@ -26,5 +29,123 @@ export interface MyContext {
     clearCart: () => void,
     setUser: (user: UserInfo) => void,
     setLang: (lang: Lang) => void,
+  }
+}
+
+export const defaultContext: MyContext = {
+  data: {
+    query: {answers: [] as QueryAnswer[]},
+    service: none as Option<ServiceContext>,
+    cart: new Cart(),
+    user: UserInfo.default,
+    lang: Lang.VN,
+  },
+  action: {
+    answer: () => {},
+    popAnswer: () => {},
+    clearAnswers: () => {},
+    getNextQuery: () => none,
+    setService: () => {},
+    setCart: () => {},
+    clearCart: () => {},
+    setUser: () => {},
+    setLang: () => {},
+  },
+}
+
+export const contextFor = (app: React.Component<AppProps, MyContext>) => {
+  const appData = () => app.state.data
+
+  const getLang = () => {
+    if (app.props.cookies) {
+      const fromCookie = app.props.cookies.get('lang')
+      return fromCookie ? Lang.fromName(fromCookie) : Lang.VN
+    }
+    return Lang.VN
+  }
+
+  const setStateIfDifferent = (newState: MyContext) => {
+    if (!_.isEqual(newState.data, app.state.data)) {
+      app.setState(newState)
+    }
+  }
+
+  const setQuery = (ans: QueryAnswer[]) => {
+    setStateIfDifferent({
+      ...app.state,
+      data: {...app.state.data, query: {answers: ans}},
+    })
+  }
+
+  const setService = (sv: Option<ServiceContext>) => {
+    setStateIfDifferent({
+      ...app.state,
+      data: {...app.state.data, service: sv},
+    })
+  }
+
+  const setCart = (c: Cart) => {
+    setStateIfDifferent({
+      ...app.state,
+      data: {...app.state.data, cart: c},
+    })
+  }
+
+  const setUser = (u: UserInfo) => {
+    setStateIfDifferent({
+      ...app.state,
+      data: {...app.state.data, user: u},
+    })
+  }
+
+  const setLang = (l: Lang) => {
+    setStateIfDifferent({
+      ...app.state,
+      data: {...app.state.data, lang: l},
+    })
+  }
+
+  return {
+    data: {
+      query: {answers: []},
+      service: none,
+      cart: new Cart(),
+      user: UserInfo.default,
+      lang: getLang(),
+    },
+    action: {
+      answer: (query: QueryInfo, ...answers: string[]) => {
+        setQuery(
+          appData().query.answers.concat(
+            answers.map(a => {
+              return {query: query, get: a}
+            }),
+          ),
+        )
+      },
+      popAnswer: () => setQuery(_.dropRight(appData().query.answers)),
+      clearAnswers: () => setQuery([]),
+      getNextQuery: (): Option<QueryInfo> => {
+        const next = appData().query.answers.length
+        return appData().service.map((sv: ServiceContext) => {
+          return sv.info.queries[next]
+        })
+      },
+      setService: (sv: ServiceInfo) => {
+        setService(
+          some({
+            group: groupOf(sv),
+            info: sv,
+          }),
+        )
+      },
+      setCart: (lastAns: string, count: number) => {
+        const chain = appData().query.answers.map(a => a.get).concat([lastAns])
+        setCart(appData().cart.set(chain, count))
+      },
+      clearCart: () => setCart(new Cart()),
+      setUser: (user: UserInfo) => setUser(user),
+      setLang: (lang: Lang) => setLang(lang),
+    },
   }
 }
